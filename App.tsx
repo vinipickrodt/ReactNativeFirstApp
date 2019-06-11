cdbc0ff637beb4ec85160dbbc7b17d43a8dc0fac
Vinicius da Mata Pickrodt <vinipickrodt@gmail.com>
2019-06-10 23:13:05 -0300

    just playing with react

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, DatePickerAndroid } from 'react-native';
 import MyComponent from './MyComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <MyComponent />
+
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={{ width: "15%", height: 50, backgroundColor: 'skyblue' }} />
+        <View style={{ width: "50%", height: 50, backgroundColor: 'powderblue', alignContent: "center", alignItems: "center", flex: 1, justifyContent: "center" }}>
+          <Text style={{ fontSize: 20, alignContent: "center", alignItems: "center", textAlignVertical: "center", textAlign: "center" }}>React Native!</Text>
+        </View>
+        <View style={{ width: "35%", height: 50, backgroundColor: 'steelblue', flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <MyComponent />
+        </View>
+      </View>
+
     </View>
   );
 }
